afc7938a6d4eff51b31611ec3d1c74ef07516245
schappim <schappi@gmail.com>
2011-08-11 20:41:39 +1000

    first commit

---
+++ Rakefile
@@ -0,0 +1,7 @@
+begin
+  require 'rubygems'
+  require 'vlad'
+  Vlad.load(:scm => :git, :app => nil, :web => nil)
+rescue LoadError
+  raise "vlad didn't load!"
+end
---
+++ deploy.rb
@@ -0,0 +1,4 @@
+set :application, "lbedb"
+set :domain, "root@49.156.19.86"
+set :deploy_to, "/home/username/appname/"
+set :repository, "/home/username/appname/git_repo"
